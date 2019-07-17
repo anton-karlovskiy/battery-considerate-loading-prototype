e2b975d1b0e5a217c4b7da1f8922178f0c8ddf3b
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-07-17 18:22:06 +0300

    implemented basic version

---
+++ src/utils/hooks.js
@@ -0,0 +1,41 @@
+
+import { useState, useEffect } from 'react';
+
+const unsupportMessage = 'The Battery Status API is not supported on this platform.';
+
+const useBatteryStatus = () => {
+  const [batteryStatus, setBatteryStatus] = useState(null);
+
+  const updateBatteryStatus = battery => {
+    setBatteryStatus(battery);
+  };
+
+  useEffect(() => {
+    if ('getBattery' in navigator) {
+      navigator.getBattery().then(battery => {
+        updateBatteryStatus(battery);
+      });
+    } else {
+      setBatteryStatus(unsupportMessage);
+    }
+  }, []);
+
+  useEffect(() => {
+    if(batteryStatus && batteryStatus !== unsupportMessage) {
+      batteryStatus.addEventListener('levelchange', updateBatteryStatus);
+      batteryStatus.addEventListener('chargingchange', updateBatteryStatus);
+      batteryStatus.addEventListener('dischargingtimechange', updateBatteryStatus);
+      batteryStatus.addEventListener('chargingtimechange', updateBatteryStatus);
+      return () => {
+        batteryStatus.removeEventListener('levelchange', updateBatteryStatus);
+        batteryStatus.removeEventListener('chargingchange', updateBatteryStatus);
+        batteryStatus.removeEventListener('dischargingtimechange', updateBatteryStatus);
+        batteryStatus.removeEventListener('chargingtimechange', updateBatteryStatus);
+      }
+    }
+  }, [batteryStatus]);
+
+  return batteryStatus;
+};
+
+export { useBatteryStatus }
